07ca23e2a0d88cb7a799e960637e1c4d0467e526
Gabriel-Aragao <gabriellamparina@gmail.com>
2021-03-07 01:55:31 -0300

    Initial configuration. Dockerfile, docker-compose, and database connection

---
+++ api/src/test/java/tk/zup/vaccine/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tk.zup.vaccine.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
