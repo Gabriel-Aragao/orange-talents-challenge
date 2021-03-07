80c4f37dd87c515c05e6f7950aab1a92efa0155b
Gabriel-Aragao <gabriellamparina@gmail.com>
2021-03-07 16:43:35 -0300

    bug fixed

--- api/src/main/java/tk/zup/vaccine/api/resource/VaccinationResource.java
+++ api/src/main/java/tk/zup/vaccine/api/resource/VaccinationResource.java
@@ -8,9 +8,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import tk.zup.vaccine.api.model.User;
 import tk.zup.vaccine.api.model.Vaccination;
 import tk.zup.vaccine.api.model.dto.VaccinationDTO;
 import tk.zup.vaccine.api.model.dto.VaccinationResponseDTO;
+import tk.zup.vaccine.api.service.UserService;
 import tk.zup.vaccine.api.service.VaccinationService;
 
 @RestController
@@ -20,17 +22,25 @@ public class VaccinationResource {
   private VaccinationService vaccinationService;
 
   @Autowired
+  private UserService userService;
+
+  @Autowired
   private ModelMapper modelMapper;
 
   @PostMapping(path = "/vaccinations")
   public ResponseEntity<VaccinationResponseDTO> create(@RequestBody VaccinationDTO vaccinationDTO){
-    
-    Vaccination vaccination = modelMapper.map(vaccinationDTO, Vaccination.class);
-    
-    Vaccination savedVaccination = vaccinationService.save(vaccination);
-    
-    VaccinationResponseDTO vaccinationResponseDTO = modelMapper.map(savedVaccination, VaccinationResponseDTO.class);
-    
-    return ResponseEntity.status(HttpStatus.CREATED).body(vaccinationResponseDTO);
+    try {
+      Vaccination vaccination = modelMapper.map(vaccinationDTO, Vaccination.class);
+      User user = userService.findById(vaccinationDTO.getPatientId()).orElseThrow();
+      vaccination.setPatient(user);
+      Vaccination savedVaccination = vaccinationService.save(vaccination);
+      
+      VaccinationResponseDTO vaccinationResponseDTO = modelMapper.map(savedVaccination, VaccinationResponseDTO.class);
+      
+      return ResponseEntity.status(HttpStatus.CREATED).body(vaccinationResponseDTO);
+      
+    } catch (Exception e) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new VaccinationResponseDTO());
+    }
   }
 }
